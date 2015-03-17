ad9fa9a036147ae574045d60f55064a63af78e57
Tom <tom_t_lee@yahoo.com>
2015-03-17 18:24:01 +0000

    Add edit and update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,18 @@ class PostsController < ApplicationController
   end
   
   def edit
+    @post = Post.find(params[:id])
   end
   
   def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      flash[:notice] = "This post has been updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
   
   private
