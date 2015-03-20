ab32b436693e7eb3de380b5756772e120f801116
Tom <tom_t_lee@yahoo.com>
2015-03-20 15:21:41 +0000

    Add New Category validation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,20 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+  
+  def create
+    @category = Category.new(category_params)  
+    
+    if @category.save
+      flash[:notice] = "Category has been created"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
