51ce69a9f0db7b68360bc9e986572d01764f8033
Tom <tom_t_lee@yahoo.com>
2015-03-20 15:11:35 +0000

    Add new categories form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   
-  resources :posts, except: :destroy
+  resources :posts, except: [:destroy]
+  resources :categories, only: [:new, :create, :show]
 end
