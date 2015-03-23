21ef6d8ed3d27fb5d9d7bb9125d84db3098e0286
Tom <tom_t_lee@yahoo.com>
2015-03-23 17:38:36 +0000

    Add datetime display helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?('http://') ? str : "http:// #{str}"
+  end
+  
+  def display_datetime(datetime_input)
+    datetime_input.strftime("%D %r %Z") # 3/23/2015 9:09am UTC
+  end
 end
