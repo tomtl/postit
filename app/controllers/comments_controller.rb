67a6c54ddee8bd9dfe766406461d43fbbbc4ea8f
Tom Lee <tom_t_lee@yahoo.com>
2015-03-26 21:00:18 -0400

    Start adding votes on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,28 @@
 class CommentsController < ApplicationController
   before_action :require_user
-  
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
     @comment.creator = current_user
-    
+
     if @comment.save
       flash[:notice] = "Your comment has been added."
       redirect_to post_path(@post)
     else
       render 'posts/show'
     end
+  end
+
+  def vote
+    @comment = Comment.find(params[:id]) # TODO: Check Params
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+
+    if @vote.valid?
+      flash[:notice] = "Your vote has been counted."
+    else
+      flash[:error] = "You can only vote once on that."
+    end
+    redirect_to :back
   end
 end
