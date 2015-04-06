230699f21393a93939c9c33f35c07879913d6e95
Tom <tom_t_lee@yahoo.com>
2015-04-06 16:44:34 +0000

    Add time zones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
   end
   
   def display_datetime(datetime_input)
+    if logged_in? && !current_user.time_zone.blank?
+      datetime_input = datetime_input.in_time_zone(current_user.time_zone)
+    end
+    
     datetime_input.strftime("%D %r %Z") # 3/23/2015 9:09am UTC
   end
   
